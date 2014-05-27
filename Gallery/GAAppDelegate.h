7a4c3d00b5e77c0a34b2ae28ff07d8763872896c
sam xu <samxu@sams-iMac.local>
2014-05-27 14:47:57 +1000

    sam first commit

---
+++ Gallery/GAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAAppDelegate.h
+//  Gallery
+//
+//  Created by sam xu on 27/05/2014.
+//  Copyright (c) 2014 SAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GalleryTests/GalleryTests.m
@@ -0,0 +1,34 @@
+//
+//  GalleryTests.m
+//  GalleryTests
+//
+//  Created by sam xu on 27/05/2014.
+//  Copyright (c) 2014 SAM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GalleryTests : XCTestCase
+
+@end
+
+@implementation GalleryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
